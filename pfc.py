44d3b9ef73ee328d3dd8e0a89b95e0b43326c140
ElRider-l <65351289+ElRider-l@users.noreply.github.com>
2020-05-15 23:10:55 +0200

    Update pfc.py

--- pfc.py
+++ pfc.py
@@ -1,1 +1,46 @@
+import random
+running = True
+list1 = ["pebble", "paper", "scissor"]
+
+while running:
+
+
+    result_ia = random.choice(list1)
+    result = input("Pebble, paper, scissors, ...")
+
+    if result_ia == "pebble" and result == str("scissor"):
+
+        print("You loose!")
+        running = False
+
+    elif result_ia == "pebble" and result == str("paper"):
+
+        print("You win!")
+        running = False
+
+    elif result_ia == "paper" and result == str("scissor"):
+
+        print("You win!")
+        running = False
+
+    elif result_ia == "scissor" and result == str("paper"):
+
+        print("You loose!")
+        running = False
+
+    elif result_ia == "scissor" and result == str("pebble"):
+
+        print("You win!")
+        running = False
+
+    elif result_ia == "paper" and result == str("pebble"):
+
+        print("You loose!")
+        running = False
+
+    else:
+
+        print("Retry putasss!")
+
+        continue
 
